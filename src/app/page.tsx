5ab1ca4b847c68943a0d21d36a9dc8df2cf35686
Jencel Earl Temporado <temporado.jencel123@gmail.com>
2025-10-31 09:46:15 +0000

    the circled card is all the way down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SortVisionLoader from "@/components/sort-vision-loader";
 
 export default function Home() {
   return (
-    <div className="flex min-h-full flex-1 flex-col items-center justify-center p-4 sm:p-6">
+    <div className="grid min-h-full flex-1 place-items-center p-4 sm:p-6">
       <SortVisionLoader />
     </div>
   );
