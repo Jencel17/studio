86a4e625dd74dbe234748c74dc1a173be5cfd01b
Jencel Earl Temporado <temporado.jencel123@gmail.com>
2025-10-31 09:43:58 +0000

    fix the main card position where i need to scoll down to see the main ca

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SortVisionLoader from "@/components/sort-vision-loader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-4 sm:p-6">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-6">
       <SortVisionLoader />
     </main>
   );
