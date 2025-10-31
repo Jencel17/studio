b1bdf1559f2af8c1e9e7d2aa1fc5dca0c6c05569
Jencel Earl Temporado <temporado.jencel123@gmail.com>
2025-10-31 09:45:16 +0000

    deep serach on why i need to scoll down to able to see the main card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import SortVisionLoader from "@/components/sort-vision-loader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-6">
+    <div className="flex min-h-full flex-1 flex-col items-center justify-center p-4 sm:p-6">
       <SortVisionLoader />
-    </main>
+    </div>
   );
 }
