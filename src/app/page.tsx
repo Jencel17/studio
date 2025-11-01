57bda137eae997e30cba8ed1ceaf0c5bbb04f9d7
Jencel Earl Temporado <temporado.jencel123@gmail.com>
2025-11-01 12:42:59 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SortVisionLoader from "@/components/sort-vision-loader";
 
 export default function Home() {
   return (
-    <div className="grid min-h-full flex-1 place-items-center p-4 sm:p-6">
+    <div className="grid min-h-screen flex-1 place-items-center p-4 sm:p-6">
       <SortVisionLoader />
     </div>
   );
