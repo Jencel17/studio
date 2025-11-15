9096f84e134c9bacbe44a0f6d6f3395b01aa15a0
Jencel Earl Temporado <temporado.jencel123@gmail.com>
2025-11-15 06:19:26 +0000

    C:\Users\Jencel\Desktop\esp32\test for recieving string\sketch_nov14a\sk

--- esp32_example_code.ino
+++ esp32_example_code.ino
@@ -1,377 +1,181 @@
-/*
-  ======================================================================================================================
-  === ESP32-CAM EXAMPLE CODE for SortVision App
-  ======================================================================================================================
-
-  This code is designed to work with the Next.js SortVision application.
-  It creates a Wi-Fi Access Point that your phone can connect to, and then runs a web server
-  that listens for commands from the app to control sorting mechanisms (servos, lights, etc.).
-
-  ----------------------------------------------------------------------------------------------------------------------
-  --- HOW TO FIX "mbedtls" COMPILATION ERRORS ---
-  ----------------------------------------------------------------------------------------------------------------------
-  If you see compilation errors related to `mbedtls_md5_starts_ret` or `genRandomMD5`, it is because of an
-  incompatibility between your ESP32 board version and the ESPAsyncWebServer library.
-
-  You must manually patch a library file.
-
-  STEP 1: Find the file "WebAuthentication.cpp"
-    - This file is inside your installed Arduino libraries.
-    - A typical path on Windows is:
-      C:\Users\[Your_Username]\Documents\Arduino\libraries\ESPAsyncWebServer\src\WebAuthentication.cpp
-    - A typical path on macOS is:
-      ~/Documents/Arduino/libraries/ESPAsyncWebServer/src/WebAuthentication.cpp
-
-  STEP 2: Replace the ENTIRE content of the file
-    - Open `WebAuthentication.cpp` in a text editor.
-    - Delete ALL the text inside it.
-    - Copy the C++ code block below and paste it into the empty file.
-    - Save the file.
-
-  STEP 3: Re-compile this sketch.
-    - The errors should now be gone.
-
-  --- START OF C++ CODE TO COPY ---
-  /*
-  Asynchronous WebServer library for Espressif MCUs
-
-  Copyright (c) 2016 Hristo Gochkov. All rights reserved.
-  This file is part of the esp8266 core for Arduino environment.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  */
-  #include "WebAuthentication.h"
-  #include <libb64/cencode.h>
-  #ifdef ESP32
-  #include "mbedtls/md5.h"
-  #include <esp_random.h>
-  #else
-  #include "md5.h"
-  #endif
-
-  // Basic Auth hash = base64("username:password")
-  bool checkBasicAuthentication(const char * hash, const char * username, const char * password){
-    if(username == NULL || password == NULL || hash == NULL)
-      return false;
-
-    size_t toencodeLen = strlen(username)+strlen(password)+1;
-    size_t encodedLen = base64_encode_expected_len(toencodeLen);
-    if(strlen(hash) != encodedLen)
-      return false;
-
-    char *toencode = new char[toencodeLen+1];
-    if(toencode == NULL){
-      return false;
-    }
-    char *encoded = new char[base64_encode_expected_len(toencodeLen)+1];
-    if(encoded == NULL){
-      delete[] toencode;
-      return false;
-    }
-    sprintf(toencode, "%s:%s", username, password);
-    if(base64_encode_chars(toencode, toencodeLen, encoded) > 0 && memcmp(hash, encoded, encodedLen) == 0){
-      delete[] toencode;
-      delete[] encoded;
-      return true;
-    }
-    delete[] toencode;
-    delete[] encoded;
-    return false;
-  }
-
-  #if defined(ESP32)
-  static bool getMD5(uint8_t * data, uint16_t len, char * md5) {
-      int i;
-      unsigned char _buf[16];
-      mbedtls_md5_context _ctx;
-
-      mbedtls_md5_init(&_ctx);
-      mbedtls_md5_starts(&_ctx);
-      mbedtls_md5_update(&_ctx, data, len);
-      mbedtls_md5_finish(&_ctx, _buf);
-      mbedtls_md5_free(&_ctx);
-
-      for(i = 0; i < 16; i++) {
-          sprintf(md5 + i * 2, "%02x", _buf[i]);
-      }
-      return true;
-  }
-  #else
-  static bool getMD5(uint8_t * data, uint16_t len, char * md5){
-    MD5_CTX ctx;
-    MD5Init(&ctx);
-    MD5Update(&ctx, data, len);
-    unsigned char digest[16];
-    MD5Final(digest, &ctx);
-    int i;
-    for(i=0; i<16; i++){
-      sprintf(md5+i*2, "%02x", digest[i]);
-    }
-    return true;
-  }
-  #endif
-
-  // Digest Auth
-  static String stringMD5(String str){
-    char buff[33];
-    getMD5((uint8_t*)str.c_str(), str.length(), buff);
-    return String(buff);
-  }
-
-  static String genRandomMD5(){
-    uint32_t r = 0;
-    char buff[33];
-  #ifdef ESP32
-    r = esp_random();
-  #else
-    r = rand();
-  #endif
-    getMD5((uint8_t*)&r, sizeof(r), buff);
-    return String(buff);
-  }
-
-  String requestDigestAuthentication(const char * realm){
-    String header = "realm=\"";
-    if(realm == NULL)
-      header.concat("asyncesp");
-    else
-      header.concat(realm);
-    header.concat( "\", qop=\"auth\", nonce=\"");
-    header.concat(genRandomMD5());
-    header.concat("\", opaque=\"");
-    header.concat(genRandomMD5());
-    header.concat("\"");
-    return header;
-  }
-
-  bool checkDigestAuthentication(const char * header, const char * method, const char * username, const char * password, const char * realm, bool passwordIsHash, const char * nonce, const char * opaque, const char * uri){
-    if(username == NULL || password == NULL || header == NULL || method == NULL){
-      //os_printf("AUTH FAIL: missing requred fields\n");
-      return false;
-    }
-
-    String myHeader = String(header);
-    int nextBreak = myHeader.indexOf(",");
-    if(nextBreak < 0){
-      //os_printf("AUTH FAIL: no variables\n");
-      return false;
-    }
-
-    String myUsername = String();
-    String myRealm = String();
-    String myNonce = String();
-    String myUri = String();
-    String myResponse = String();
-    String myQop = String();
-    String myNc = String();
-    String myCnonce = String();
-
-    myHeader += ", ";
-    do {
-      String avLine = myHeader.substring(0, nextBreak);
-      avLine.trim();
-      myHeader = myHeader.substring(nextBreak+1);
-      nextBreak = myHeader.indexOf(",");
-
-      int eqSign = avLine.indexOf("=");
-      if(eqSign < 0){
-        //os_printf("AUTH FAIL: no = sign\n");
-        return false;
-      }
-      String varName = avLine.substring(0, eqSign);
-      avLine = avLine.substring(eqSign + 1);
-      if(avLine.startsWith("\"")){
-        avLine = avLine.substring(1, avLine.length() - 1);
-      }
-
-      if(varName.equals("username")){
-        if(!avLine.equals(username)){
-          //os_printf("AUTH FAIL: username\n");
-          return false;
-        }
-        myUsername = avLine;
-      } else if(varName.equals("realm")){
-        if(realm != NULL && !avLine.equals(realm)){
-          //os_printf("AUTH FAIL: realm\n");
-          return false;
-        }
-        myRealm = avLine;
-      } else if(varName.equals("nonce")){
-        if(nonce != NULL && !avLine.equals(nonce)){
-          //os_printf("AUTH FAIL: nonce\n");
-          return false;
-        }
-        myNonce = avLine;
-      } else if(varName.equals("opaque")){
-        if(opaque != NULL && !avLine.equals(opaque)){
-          //os_printf("AUTH FAIL: opaque\n");
-          return false;
-        }
-      } else if(varName.equals("uri")){
-        if(uri != NULL && !avLine.equals(uri)){
-          //os_printf("AUTH FAIL: uri\n");
-          return false;
-        }
-        myUri = avLine;
-      } else if(varName.equals("response")){
-        myResponse = avLine;
-      } else if(varName.equals("qop")){
-        myQop = avLine;
-      } else if(varName.equals("nc")){
-        myNc = avLine;
-      } else if(varName.equals("cnonce")){
-        myCnonce = avLine;
-      }
-    } while(nextBreak > 0);
-
-    String ha1 = (passwordIsHash) ? String(password) : stringMD5(myUsername + ":" + myRealm + ":" + String(password));
-    String ha2 = stringMD5(String(method) + ":" + myUri);
-    String response = stringMD5(ha1 + ":" + myNonce + ":" + myNc + ":" + myCnonce + ":" + myQop + ":" + ha2);
-
-    if(myResponse.equals(response)){
-      //os_printf("AUTH SUCCESS\n");
-      return true;
-    }
-
-    //os_printf("AUTH FAIL: password\n");
-    return false;
-  }
-
-  --- END OF C++ CODE TO COPY ---
-*/
 
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 
 // =======================================================================
-// === 1. CONFIGURE YOUR SETTINGS
+// IMPORTANT: PLEASE READ
+// =======================================================================
+// This Arduino sketch is the counterpart to the SortVision Next.js web application.
+// It is designed to run on an ESP32 microcontroller.
+//
+// HOW TO USE:
+// 1. HARDWARE SETUP:
+//    - Connect your LEDs to the GPIO pins defined below (PLASTIC_LED_PIN, etc.).
+//      If you don't have LEDs, you can watch the Serial Monitor for output.
+//    - Power your ESP32.
+//
+// 2. SOFTWARE SETUP:
+//    - Open this file in the Arduino IDE.
+//    - Go to "Tools" > "Board" and select your specific ESP32 board model.
+//    - Go to "Tools" > "Port" and select the COM port your ESP32 is connected to.
+//    - UPDATE WIFI CREDENTIALS: Change the `ssid` and `password` variables below
+//      to match your local WiFi network.
+//
+// 3. LIBRARIES:
+//    - Make sure you have the "ESPAsyncWebServer" library installed.
+//    - In the Arduino IDE, go to "Sketch" > "Include Library" > "Manage Libraries...".
+//    - Search for "ESPAsyncWebServer" and install it.
+//    - This library also requires the "AsyncTCP" library. Search for and install it as well.
+//
+// 4. UPLOAD & RUN:
+//    - Click the "Upload" button in the Arduino IDE.
+//    - After uploading, open the "Serial Monitor" (Tools > Serial Monitor) and
+//      set the baud rate to 115200.
+//    - The ESP32 will print its IP address once connected to your WiFi.
+//    - Enter this IP address into the SortVision app's settings panel.
 // =======================================================================
 
-// --- Wi-Fi Access Point Settings ---
-// This is the name of the Wi-Fi network the ESP32 will create.
-// Your phone will connect to this network.
-const char* ssid = "SortVision-ESP32"; 
-// Leave the password blank for an open network.
-const char* password = ""; 
 
-// --- Pin Definitions ---
+// --- PIN DEFINITIONS ---
 // IMPORTANT: Change these pin numbers to match your ESP32's wiring.
-const int PLASTIC_PIN = 13; // Pin for the "Plastic" sorting mechanism
-const int METAL_PIN = 12;   // Pin for the "Metal" sorting mechanism
-const int PAPER_PIN = 14;   // Pin for the "Paper" sorting mechanism
-const int LIGHT_PIN = 15;   // Pin to control the lighting
+const int PLASTIC_LED_PIN = 13;
+const int METAL_LED_PIN   = 12;
+const int PAPER_LED_PIN   = 14;
+const int LIGHT_PIN       = 27; // General purpose light/flash
 
-// =======================================================================
-// === 2. SERVER AND LOGIC (Usually no need to edit below this line)
-// =======================================================================
+// --- WIFI CREDENTIALS ---
+// IMPORTANT: Replace with your WiFi network SSID and password.
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
 
-// Create an instance of the server on port 80
+// Create an instance of the Asynchronous Web Server on port 80
 AsyncWebServer server(80);
 
+void connectToWiFi() {
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+    delay(500);
+    Serial.print(".");
+    attempts++;
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\nWiFi connected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\nFailed to connect to WiFi. Please check credentials and restart.");
+  }
+}
+
+// Function to handle the /sort command
 void handleSortRequest(AsyncWebServerRequest *request) {
-  // Check if a "class" parameter exists in the request (e.g., /sort?class=PLASTIC)
+  String material = "";
   if (request->hasParam("class")) {
-    String material = request->getParam("class")->value();
-    
-    // Convert the received material string to uppercase to make comparisons case-insensitive
-    material.toUpperCase(); 
+    material = request->getParam("class")->value();
+    material.toUpperCase(); // Ensure consistency
 
     Serial.print("Received sort command for: ");
     Serial.println(material);
 
+    // Turn off all LEDs first
+    digitalWrite(PLASTIC_LED_PIN, LOW);
+    digitalWrite(METAL_LED_PIN, LOW);
+    digitalWrite(PAPER_LED_PIN, LOW);
+
+    // Turn on the correct LED
     if (material == "PLASTIC") {
-      Serial.println("Activating PLASTIC mechanism.");
-      digitalWrite(PLASTIC_PIN, HIGH);
-      delay(1000); // Keep it active for 1 second
-      digitalWrite(PLASTIC_PIN, LOW);
+      digitalWrite(PLASTIC_LED_PIN, HIGH);
+      Serial.println("Activating PLASTIC sorter mechanism.");
     } else if (material == "METAL") {
-      Serial.println("Activating METAL mechanism.");
-      digitalWrite(METAL_PIN, HIGH);
-      delay(1000);
-      digitalWrite(METAL_PIN, LOW);
+      digitalWrite(METAL_LED_PIN, HIGH);
+      Serial.println("Activating METAL sorter mechanism.");
     } else if (material == "PAPER") {
-      Serial.println("Activating PAPER mechanism.");
-      digitalWrite(PAPER_PIN, HIGH);
-      delay(1000);
-      digitalWrite(PAPER_PIN, LOW);
+      digitalWrite(PAPER_LED_PIN, HIGH);
+      Serial.println("Activating PAPER sorter mechanism.");
     } else {
-      // If the class is not recognized
-      Serial.println("Unknown material class received.");
-      request->send(400, "text/plain", "Error: Unknown class");
+      Serial.println("Unknown material received.");
+      request->send(400, "text/plain", "Unknown material");
       return;
     }
-    // If successful, send a confirmation response back to the app
+
     request->send(200, "text/plain", "OK: Sorted " + material);
+
+    // Optional: Turn the LED off after a delay
+    delay(1000);
+    digitalWrite(PLASTIC_LED_PIN, LOW);
+    digitalWrite(METAL_LED_PIN, LOW);
+    digitalWrite(PAPER_LED_PIN, LOW);
+    
   } else {
-    // If the 'class' parameter is missing
-    Serial.println("Error: 'class' parameter missing in sort request.");
-    request->send(400, "text/plain", "Error: Missing 'class' parameter");
+    Serial.println("Sort command received without 'class' parameter.");
+    request->send(400, "text/plain", "Bad Request: Missing 'class' parameter");
   }
 }
 
+// Function to handle the /light command
 void handleLightRequest(AsyncWebServerRequest *request) {
-  // Check if a "state" parameter exists (e.g., /light?state=ON)
   if (request->hasParam("state")) {
     String state = request->getParam("state")->value();
-    state.toUpperCase(); // Case-insensitive matching
-
+    state.toUpperCase();
     if (state == "ON") {
-      Serial.println("Turning light ON.");
       digitalWrite(LIGHT_PIN, HIGH);
-      request->send(200, "text/plain", "OK: Light is ON");
+      Serial.println("Light turned ON");
+      request->send(200, "text/plain", "OK: Light ON");
     } else if (state == "OFF") {
-      Serial.println("Turning light OFF.");
       digitalWrite(LIGHT_PIN, LOW);
-      request->send(200, "text/plain", "OK: Light is OFF");
+      Serial.println("Light turned OFF");
+      request->send(200, "text/plain", "OK: Light OFF");
     } else {
-      Serial.println("Error: Invalid 'state' for light command.");
-      request->send(400, "text/plain", "Error: Invalid 'state'. Use ON or OFF.");
+      request->send(400, "text/plain", "Bad Request: Invalid state");
     }
   } else {
-    Serial.println("Error: 'state' parameter missing in light request.");
-    request->send(400, "text/plain", "Error: Missing 'state' parameter");
+    request->send(400, "text/plain", "Bad Request: Missing 'state' parameter");
   }
+}
+
+void handleNotFound(AsyncWebServerRequest *request) {
+  Serial.print("Client requested unknown URL: ");
+  Serial.println(request->url());
+  request->send(404, "text/plain", "Not found");
 }
 
 void setup() {
+  // Start the Serial Monitor
   Serial.begin(115200);
-  pinMode(PLASTIC_PIN, OUTPUT);
-  pinMode(METAL_PIN, OUTPUT);
-  pinMode(PAPER_PIN, OUTPUT);
+  Serial.println("\nSortVision ESP32 Initializing...");
+
+  // Set up the GPIO pins for the LEDs
+  pinMode(PLASTIC_LED_PIN, OUTPUT);
+  pinMode(METAL_LED_PIN, OUTPUT);
+  pinMode(PAPER_LED_PIN, OUTPUT);
   pinMode(LIGHT_PIN, OUTPUT);
 
-  digitalWrite(PLASTIC_PIN, LOW);
-  digitalWrite(METAL_PIN, LOW);
-  digitalWrite(PAPER_PIN, LOW);
+  // Ensure all LEDs are off at startup
+  digitalWrite(PLASTIC_LED_PIN, LOW);
+  digitalWrite(METAL_LED_PIN, LOW);
+  digitalWrite(PAPER_LED_PIN, LOW);
   digitalWrite(LIGHT_PIN, LOW);
 
-  WiFi.softAP(ssid, password);
-  Serial.println("Access Point started");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.softAPIP());
+  // Connect to the WiFi network
+  connectToWiFi();
 
-  // Define server routes
+  // Define the server routes
   server.on("/sort", HTTP_GET, handleSortRequest);
-  server.on("/light", HTTP_GET, handleLightRequest);
-  
+  server.on("/light", HTTP_get, handleLightRequest);
+
+  // Define a catch-all for 404 Not Found errors
+  server.onNotFound(handleNotFound);
+
   // Start the server
   server.begin();
-  Serial.println("Web server started");
+  Serial.println("HTTP server started. Awaiting commands from the app.");
 }
 
 void loop() {
-  // The main loop is empty because the async web server handles requests in the background.
-  // Using delay() in here can cause the server to crash.
+  // The loop is intentionally kept empty.
+  // The ESPAsyncWebServer library handles client requests in the background.
+  // Using delay() in the loop can cause the web server to become unresponsive.
 }
