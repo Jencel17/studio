a4ff2b738cd4c476ba5fb6659c47f19bf36e3224
Jencel Earl Temporado <temporado.jencel123@gmail.com>
2025-11-20 16:17:28 +0000

    reduce unused java script also fix the contrass on the ui

--- src/components/icons.tsx
+++
@@ -1,5 +0,0 @@
-import { cn } from "@/lib/utils";
-
-// This file is reserved for custom SVG icons.
-// The hardcoded icons for specific categories have been removed
-// to allow the UI to dynamically adapt to any loaded Teachable Machine model.
